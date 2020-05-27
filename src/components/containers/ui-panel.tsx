90bf792b73860e4f64d59dbc7b9d5d25c76025ef
Adrian Hernandez-Mendez <aahdezm@gmail.com>
2020-05-28 00:14:52 +0200

    Add box wrapper to the UIPanel

--- src/components/containers/ui-panel.tsx
+++ src/components/containers/ui-panel.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { UIFactory } from '../ui-factory';
-import { UIElement } from '../../types';
+import { UIElement, UIElementProps } from '../../types';
 import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 
-interface UIPanelProps {
+interface UIPanelProps extends UIElementProps {
   elements: Array<UIElement>;
   variant?: 'elevation' | 'outlined';
   elevation?: number;
@@ -11,15 +12,15 @@ interface UIPanelProps {
 }
 
 export function UIPanel({ elements, ...props }: UIPanelProps): JSX.Element {
-  const variant = props.variant || 'elevation';
-  const elevation = props.elevation || 1;
-  const square = props.square || false;
+  const { variant = 'elevation', elevation = 1, square = false, ...boxProps } = props;
 
   return (
-    <Paper square={square} elevation={elevation} variant={variant}>
-      {elements.map((el, index) => (
-        <UIFactory key={index} element={el} />
-      ))}
-    </Paper>
+    <Box {...boxProps}>
+      <Paper square={square} elevation={elevation} variant={variant}>
+        {elements.map((el, index) => (
+          <UIFactory key={index} element={el} />
+        ))}
+      </Paper>
+    </Box>
   );
 }
